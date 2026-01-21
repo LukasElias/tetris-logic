88c71fbed02e06b67edad44a40cb9d1c7507da72
Lukas Elias Lund Majland <lukaslundmajland@gmail.com>
2026-01-21 16:52:26 +0100

    Wrote some basic code, haven't gotten to use ratatui yet

--- examples/ratatetris/src/main.rs
+++ examples/ratatetris/src/main.rs
@@ -1,3 +1,28 @@
+use tetris::{Game, Input, InputAction, Render, GameState};
+use rand::rng;
+
 fn main() {
-    println!("Hello, world!");
+    let input = InputDevice;
+    let render = Renderer;
+    let rng = rng();
+
+    let mut game = Game::new(input, render, rng);
+
+    game.tick();
+}
+
+struct InputDevice;
+
+impl Input for InputDevice {
+    fn next_input(&mut self) -> Option<InputAction> {
+        None
+    }
+}
+
+struct Renderer;
+
+impl Render for Renderer {
+    fn render(&mut self, state: &GameState) {
+        println!("{:?}", state);
+    }
 }
